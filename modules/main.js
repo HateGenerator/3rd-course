72ffd5c3d59d24522c3b26d09c2188fc30c03d40
Sam <kinotagil@gmail.com>
2025-03-20 12:00:31 +0500

    доделал по советам

--- modules/main.js
+++ modules/main.js
@@ -1,5 +1,8 @@
 import { renderComments } from "./render.js";
 import { addComment } from "./addComment.js";
+import { addEventListeners } from "./eventHandlers.js";
+
+addEventListeners(comments, commentList, commentInput, nameInput, renderComments);
 
 const nameInput = document.querySelector(".add-form-name");
 const commentInput = document.querySelector(".add-form-text");
